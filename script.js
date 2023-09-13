4cdb11b2acf75f0c0bb5666d7e5b1fc8b9bf9df0
jaelsonsantos1 <jaelsons860@gmail.com>
2023-09-13 08:06:24 -0300

    Adicionado funções de buscarImagens e Recarregar

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const { google } = require("googleapis");
 const express = require("express");
 const app = express();
 const port = 3000; // Escolha a porta que desejar
-const fileList = [];
+let fileList = []; // Inicialmente vazia
 
 const privatekey = require("./sliderproject-398111-54bcc9b7ec0d.json");
 
@@ -13,52 +13,62 @@ const jwtClient = new google.auth.JWT(
   ["https://www.googleapis.com/auth/drive"]
 );
 
-jwtClient.authorize((err, tokens) => {
-  if (err) {
-    console.error(err);
-    return;
-  }
-
-  // Agora você está autenticado e pode usar a API do Google Drive.
-
-  // Listar imagens no Google Drive
-  const drive = google.drive({ version: "v3", auth: jwtClient });
-
-  // Criando uma lista de imagens
-
-  drive.files.list(
-    {
-      q:
-        "'" +
-        "1f6riyd80tqobA9s2lvU9uHg9t4htZGeB" +
-        "' in parents and trashed=false",
-      fields: "files(name, webContentLink)",
-    },
-    (err, res) => {
-      if (err) {
-        console.error(err);
-        return;
-      }
-
-      res.data.files.forEach((file) => {
-        fileList.push(file);
-      });
-
-      app.get('/', (req, res) => {
-        // Renderizando um arquivo HTML
-        res.sendFile(__dirname + '/index.html');
-      });
-      
-      app.get('/imagens', (req, res) => {
-        res.json(fileList);
-      });
-  
-      // Iniciar o servidor
-      app.listen(port, () => {
-        console.log(`Servidor Express em execução em http://localhost:${3000}`);
-      });
-
+// Função para buscar as imagens e guardar na lista
+function buscarImagens(callback) {
+  jwtClient.authorize((err, tokens) => {
+    if (err) {
+      console.error(err);
+      return;
     }
-  );
+
+    const drive = google.drive({ version: "v3", auth: jwtClient });
+
+    drive.files.list(
+      {
+        q: "'" + "1f6riyd80tqobA9s2lvU9uHg9t4htZGeB" + "' in parents and trashed=false",
+        fields: "files(name, webContentLink)",
+      },
+      (err, res) => {
+        if (err) {
+          console.error(err);
+          return;
+        }
+
+        const newFileList = [];
+
+        res.data.files.forEach((file) => {
+          newFileList.push(file);
+        });
+
+        fileList = newFileList; // Atualiza a lista global de imagens
+
+        if (typeof callback === "function") {
+          callback(fileList); // Chama a função de callback com a lista de imagens
+        }
+      }
+    );
+  });
+}
+
+// Função para atualizar a lista de imagens
+function atualizarListaImagens(req, res) {
+  buscarImagens((imagens) => {
+    res.json(imagens);
+  });
+}
+
+// Rota para buscar as imagens
+app.get('/imagens', atualizarListaImagens);
+
+app.get('/', (req, res) => {
+  // Renderizar um arquivo HTML
+  res.sendFile(__dirname + '/index.html');
 });
 
+// Iniciar o servidor
+app.listen(port, () => {
+  console.log(`Servidor Express em execução em http://localhost:${port}`);
+});
+
+// Inicialmente, buscar as imagens e guardar na lista
+buscarImagens();
